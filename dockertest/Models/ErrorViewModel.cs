6eef04615c2e71a36a78cf994397648fc62f5d15
田伟 <tianwei1@tiens.com>
2019-03-25 16:12:58 +0800

    init

---
+++ dockertest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dockertest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
